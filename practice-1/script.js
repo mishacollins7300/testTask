46466d0432a5ad26110e7b359a42389dfc763293
Эмилия <e.muraveva@ritg.ru>
2023-09-21 23:25:52 +0400

    Обработка загрузки фотографии

--- practice-1/script.js
+++ practice-1/script.js
@@ -2,3 +2,14 @@ function openModal() {
     let modal = document.getElementById('modal-window');
     modal.classList.add('modal-window_open');
 }
+
+function handleFileSelect (evt) {
+    // Loop through the FileList and render image files as thumbnails.
+    for (const file of evt.target.files) {
+
+        // Render thumbnail.
+        const img = document.getElementById('list')
+        const src = URL.createObjectURL(file)
+        img.setAttribute('src', src );
+    }
+}
